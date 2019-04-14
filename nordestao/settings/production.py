91f7735a1143cee5786cbe849c34f713a01e8eb4
Marcus Ferreira <marcus.vini2@hotmail.com>
2019-04-14 19:14:00 -0300

    Muda caminho dos settings

--- nordestao/settings/production.py
+++ nordestao/settings/production.py
@@ -113,7 +113,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    'static',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
 
 if False:
     sentry_sdk.init("your dsn", integrations=[DjangoIntegration()])
